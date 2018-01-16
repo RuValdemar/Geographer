59c80932a3fe88ac695370b4854d2a12efab3d12
Rubens <ruvaldemar@gmail.com>
2018-01-15 19:12:41 -0500

    Demo sobre el uso de la libreria GEOGRAPHER de PHP

--- index.php
+++ index.php
@@ -4,19 +4,14 @@ use MenaraSolutions\Geographer\Earth;
 use MenaraSolutions\Geographer\Country;
 
 $earth = new Earth();
-#dd($earth); /*dd debugea*/
-#dd($earth->getCountries()->toArray());
 
 $countries = $earth->getCountries()->toArray();
 
-$country = $earth->findOneByCode('BR'); /*Devuelve la información del pais*/
-#dd($country->toArray());
+$country = $earth->findOneByCode('BR'); 
 
 $states = $country->getStates();
-#dd($states->toArray());
 
 $state = $states->findOne(['code' => 'BR-GO']);
-#dd($state->toArray());
 
 $cities = $state->getCities();
 dd($cities->toArray());
